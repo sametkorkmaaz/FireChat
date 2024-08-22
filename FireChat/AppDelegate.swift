caeb386e044211da723c1ff7c4211352371735ac
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2024-08-22 15:34:10 +0300

    a

--- FireChat/AppDelegate.swift
+++ FireChat/AppDelegate.swift
@@ -7,14 +7,23 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseFirestore
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         FirebaseApp.configure()
-        Thread.sleep(forTimeInterval: 1)
+        let db = Firestore.firestore()
+        print(db)
+        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.resignOnTouchOutside = true
+        
         return true
     }
 
